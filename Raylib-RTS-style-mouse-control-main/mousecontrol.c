4187459fc80649fd4fbe2d4af3238e080ce92454
Aaron <117947303+aaronbehan@users.noreply.github.com>
2025-01-02 15:57:29 +0000

    Update mousecontrol.c
    Enemies can be added easily and are not player controllable
    Orange indicator to show RMB click registered

--- Raylib-RTS-style-mouse-control-main/mousecontrol.c
+++ Raylib-RTS-style-mouse-control-main/mousecontrol.c
@@ -1,43 +1,19 @@
 #include "raylib.h"
-#include "raymath.h"
+#include "structs.h"
+#include "functions.h"
+#include "timer.h"
 
-#define FLT_MAX     340282346638528859811704183484516925440.0f     // Maximum value of a float, from bit pattern 01111111011111111111111111111111. I think Ray used this in the event of a rare edge case
+#define FLT_MAX     340282346638528859811704183484516925440.0f  // Maximum value of a float, from bit pattern 01111111011111111111111111111111. Necessary for ground detection, but cannot discern why yet
 
-//  debugging time. units only have to be selected once for their moveunit status to be set permantly to true
-
-// Player units which can be controlled
-typedef struct Unit {
-    Vector3 unitPos;  // X = East and West. Y = Up and Down. Z = North and South.
-    Vector3 waypoint; 
-    Vector3 unitSize;
-    float unitSpeed;
-    bool selected;
-    bool moveUnit;
-    Color color;
-} Unit;
-
-BoundingBox createBoundingBox(Vector3 startPoint, Vector3 endPoint) // Creates a bounding box using its two furthest corners
-{
-    BoundingBox boundingBox = { 0 };
-
-    if (startPoint.x <= endPoint.x) boundingBox.min.x = startPoint.x, boundingBox.max.x = endPoint.x;  // Expected mouse drag direction
-    else if (startPoint.x > endPoint.x) boundingBox.min.x = endPoint.x, boundingBox.max.x = startPoint.x;  // Unexpected 
-    
-    if (startPoint.z <= endPoint.z) boundingBox.min.z = startPoint.z, boundingBox.max.z = endPoint.z;  // Expected mouse drag direction
-    else if (startPoint.z > endPoint.z) boundingBox.min.z = endPoint.z, boundingBox.max.z = startPoint.z;  // Unexpected
-
-    boundingBox.min.y = 0.0f;
-    boundingBox.max.y = 0.5f; // Arbitrary height value 
-    
-    return boundingBox;
-}
+// upon right click w enemy and eventual collission w enemy, enemy.targeted.true unless right clicked away
+// 
 
 int main(void)
 {
     // Screen
     const int screenWidth = 1400;
     const int screenHeight = 850;
-    InitWindow(screenWidth, screenHeight, "raylib concepts and experimentation");
+    InitWindow(screenWidth, screenHeight, "window title");
 
     // Camera
     Camera camera = { { 0.0f, 10.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 };
@@ -52,19 +28,28 @@ int main(void)
 
     RayCollision objectCollision = { 0 };  // For casting a ray at an OBJECT. Stores collision hit info
     RayCollision groundCollision = { 0 };  // For casting a ray at the GROUND in order to return 3D coordinates 
-    Vector3 wayPoint = { 0 };  // Saves a 3D coordinate. Used for unit control 
 
-    #define NUMBER_OF_PLAYER_UNITS 2
+    // Timer related
+    Timer timer = { 0 };
+    float duration = 1.0f;  // roughly 1 second 
+    int ticker = 0;  
+
+    #define NUMBER_OF_PLAYER_UNITS 1  // VERY IMPORTANT TO KEEP THIS UPDATED
 
     // Player controlled units
-    Unit playerUnit1 = { { 0.0f, 1.0f, -4.0f }, { 0.0f, 1.0f, -4.0f }, { 1.0f, 2.0f, 1.0f }, 0.01f, false, false, GRAY}; 
-    Unit playerUnit2 = { { 4.0f, 1.0f, 3.0f }, { 4.0f, 1.0f, 3.0f }, { 1.0f, 2.0f, 1.0f }, 0.01f, false, false, ORANGE}; 
+    Unit playerUnit1 = { { 0.0f, 1.0f, -4.0f }, { 0.0f, 1.0f, -4.0f }, { 1.0f, 2.0f, 1.0f }, 0.05f, 10, 3, 1, false, false, false, GRAY}; 
+    // Unit playerUnit2 = { { 4.0f, 1.0f, 3.0f }, { 4.0f, 1.0f, 3.0f }, { 1.0f, 2.0f, 1.0f }, 0.01f, false, false, ORANGE}; 
 
-    Unit playerUnits[NUMBER_OF_PLAYER_UNITS] = {playerUnit1, playerUnit2};
+    Unit playerUnits[NUMBER_OF_PLAYER_UNITS] = {playerUnit1};
+
+    // Enemies
+    Unit enemyUnit1 = { { -3.0f, 1.0f, 5.0f }, { 0.0f, 1.0f, -4.0f }, { 1.0f, 2.0f, 1.0f }, 0.05f, 10, 3, 1, false, false, false, PURPLE};
+
+    Unit enemyUnits[NUMBER_OF_PLAYER_UNITS] = {enemyUnit1};
 
     // For drawing a visual aid rectangle with the mouse
     Vector2 saveMousePos2D = { 0 };
-    // For drawing a bounding box in 3D space behind the above rectangle. Cannot draw the bounding box until mouse is released because user may drag upwards or downwards, left or right, and we cannot know where to put the min and the max until we have defined the intended collision space
+    // For drawing a bounding box in 3D space behind the above rectangle
     Vector3 save3DMouseStartPos = { 0 };
     Vector3 save3DMouseEndPos = { 0 };
 
@@ -75,6 +60,9 @@ int main(void)
     // Main game loop -------------------------------------------------------------------------------
     while (!WindowShouldClose())
     {
+        UpdateTimer(&timer);  // With every cycle, timer->Lifetime is deducted by GetFrameTime as long as it is greater than 0
+        if (TimerDone(&timer)) StartTimer(&timer, duration), ticker++;
+
         // Camera controls
         if (IsKeyDown(KEY_W)) camera.position.z -= cameraSpeed, camera.target.z -= cameraSpeed;
         if (IsKeyDown(KEY_A)) camera.position.x -= cameraSpeed, camera.target.x -= cameraSpeed;
@@ -141,6 +129,11 @@ int main(void)
             DrawRectangleLines(saveMousePos2D.x, saveMousePos2D.y, GetMousePosition().x - saveMousePos2D.x, GetMousePosition().y - saveMousePos2D.y, BLUE);
         }
 
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+        {
+            DrawCircle(GetMousePosition().x, GetMousePosition().y, 20.0, ORANGE);
+        }
+
         if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))  // Move selected units with RMB. This is also how we will select targets for my units
         {
             for (int i = 0; i < NUMBER_OF_PLAYER_UNITS; i++)  // ITERATE THROUGH ALL UNITS
@@ -149,6 +142,8 @@ int main(void)
                 {
                         playerUnits[i].waypoint.x = groundCollision.point.x;  // Setting a static coordinate for units to follow
                         playerUnits[i].waypoint.z = groundCollision.point.z;
+                        // when deciding on animations, you could make a small bounding box around each waypoint and tie idling/attacking to that
+                        // perhaps just rounding the huge floats is a better idea so that equillibrium can be attained. if waypoint == unitpos, play animation x
                 }
             }
         }
@@ -162,6 +157,8 @@ int main(void)
             if (playerUnits[i].unitPos.z > playerUnits[i].waypoint.z) playerUnits[i].unitPos.z -= playerUnits[i].unitSpeed;
         }
 
+
+
         // Draw
         BeginDrawing();
 
@@ -170,18 +167,21 @@ int main(void)
             BeginMode3D(camera);
 
                 // Highlight them if they are selected/hit
-                for (int i = 0; i < NUMBER_OF_PLAYER_UNITS; i++) {  // iterate through the units
+                for (int i = 0; i < NUMBER_OF_PLAYER_UNITS; i++) {  // Iterate through the player units
 
                     DrawCube(playerUnits[i].unitPos, playerUnits[i].unitSize.x, playerUnits[i].unitSize.y, playerUnits[i].unitSize.z, playerUnits[i].color);  // unit 1
                     DrawCubeWires(playerUnits[i].unitPos, playerUnits[i].unitSize.x, playerUnits[i].unitSize.y, playerUnits[i].unitSize.z, DARKGRAY);
                 
-                    if (playerUnits[i].selected) // to check which of them are selected. then draw a wireframe around those that are
+                    if (playerUnits[i].selected) // To check which of them are selected. then draw a wireframe around those that are
                     {
                         DrawCubeWires(playerUnits[i].unitPos, playerUnits[i].unitSize.x, playerUnits[i].unitSize.y, playerUnits[i].unitSize.z, MAROON);
                         DrawCubeWires(playerUnits[i].unitPos, playerUnits[i].unitSize.x + 0.2f, playerUnits[i].unitSize.y + 0.2f, playerUnits[i].unitSize.z + 0.2f, GREEN);
                     }
                 }
 
+                DrawCube(enemyUnits[0].unitPos, enemyUnits[0].unitSize.x, enemyUnits[0].unitSize.y, enemyUnits[0].unitSize.z, enemyUnits[0].color);  // unit 1
+                DrawCubeWires(enemyUnits[0].unitPos, enemyUnits[0].unitSize.x, enemyUnits[0].unitSize.y, enemyUnits[0].unitSize.z, DARKGRAY);
+                
                 // This lets me see where the invisible bounding box will be created for debugging purposes
                 DrawCube((Vector3){save3DMouseStartPos.x, 0.0f, save3DMouseStartPos.z}, 0.1f, 0.1f, 0.1f, GRAY);
                 DrawCube((Vector3){save3DMouseEndPos.x, 0.0f, save3DMouseEndPos.z}, 0.1f, 0.1f, 0.1f, GRAY);
@@ -204,9 +204,8 @@ int main(void)
             if (showDebug) 
             {
                 DrawFPS(10, 10);
-                DrawText(TextFormat("playerUnits[0].moveUnit = %d,\n\nplayerUnits[0].selected= %d", playerUnits[0].moveUnit, playerUnits[0].selected), 30, 20, 30, LIGHTGRAY);
                 DrawText(TextFormat("playerUnits[0].unitPos.x = %f,\n\nplayerUnits[0].unitPos.z= %f", playerUnits[0].unitPos.x, playerUnits[0].unitPos.z), 30, 220, 30, LIGHTGRAY);
-                DrawText(TextFormat("wayPointX = %f,\n\nZ= %f", wayPoint.x, wayPoint.z), 30, 140, 30, LIGHTGRAY);
+                DrawText(TextFormat("ticker= %d", ticker), 30, 140, 30, LIGHTGRAY);
             }
 
         EndDrawing();
