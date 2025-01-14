d38dca608ed20e87fb4f537b577d14f27952e0f7
Aaron <117947303+aaronbehan@users.noreply.github.com>
2025-01-14 16:08:29 +0000

    Update collision.c
    Revised collision resolution changed to be dependent upon pushing the player out of the obstacle, rather than returning player to its original position. Returning player to previously saved coordinate would cause the player to get stuck against the wall.
    
    Pushing the player away in the correct direction allows for another direction to still move the player, albeit slower. It also saves us having to remember one extra variable.

--- collision/collision.c
+++ collision/collision.c
@@ -1,85 +1,86 @@
+#include <stddef.h>  // for NULL definition
+#include <stdlib.h>
 #include "raylib.h"
-// I could feed in the player coordinates as arg 1, then have a super sized bounding box/sphere to detect other bounding boxes which could 
-// prioritise which bounding boxes I need to check collisions for
+#include "structs.h"
+#include "functions.h"
+#include "math.h"
+
+// player can get stuck on the corner of a cube if it collides in the correct location. still unsure of cause
+
+#define TILE_SIZE 1.0f
+
+void resolveCollision(Unit *unit, Vector3 obstacle) // should only be accessed during a collision
+{
+    // make a child struct? the parent struct covers both unit and wall and has two members: position and size. that way this function can work for both enemy and wall collision
+
+    if (unit->position.z < obstacle.z - 1) // player above
+    {
+        unit->position.z -= unit->speed;
+    }
+    if (unit->position.z > obstacle.z + 1) // player below
+    {
+        unit->position.z += unit->speed;
+    }
+    if (unit->position.x < obstacle.x - 1) // player on left
+    {
+        unit->position.x -= unit->speed;
+    } 
+    if (unit->position.x > obstacle.x + 1) // player on right
+    {
+        unit->position.x += unit->speed;
+    }
+}
 
 int main(void)
 {
     const int screenWidth = 1400;
     const int screenHeight = 850;
 
-    InitWindow(screenWidth, screenHeight, "raylib [models] example - box collisions");
-    // InitAudioDevice();
-    // Sound sound = LoadSound("stringpluck.wav");
+    InitWindow(screenWidth, screenHeight, "quadtree based collision detection");
 
-    // Define the camera to look into our 3d world 
-    // { {X,Y,Z}, {}, {}, Distance away?, ? }
     Camera camera = { { 0.0f, 10.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 };
 
-    Vector3 playerPos = { 0.0f, 1.0f, -4.0f }; // X = West and East. Y = Up and Down. Z = North and South. what if we just turned this into the bounding box :|
-    Vector3 playerSize = { 1.0f, 2.0f, 1.0f };
-    float playerSpeed = 0.2f;
+    Unit playerUnit1 = { { -4.0f, 1.0f, -4.0f }, { 0 }, { 1.0f, 2.0f, 1.0f }, { 0 }, 0.1f, 0.5f, 40, 40, 6, false, 0, false, 0, RED};
 
-    Color playerColor = GREEN;
+    Wall wall = {{ 0.0f, 1.0f, 0.0f }, 1, 20, false, GRAY};
 
-    Vector3 enemyBoxPos = { -4.0f, 1.0f, 0.0f };
-    Vector3 enemyBoxSize = { 2.0f, 2.0f, 2.0f };
-
-    Vector3 oldPlayerPos = { 0, 0, 0 };
     bool collision = false;
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        oldPlayerPos = playerPos;
-
         // Move player 
-        if (IsKeyDown(KEY_RIGHT)) playerPos.x += playerSpeed;
-        if (IsKeyDown(KEY_LEFT)) playerPos.x -= playerSpeed;
-        if (IsKeyDown(KEY_DOWN)) playerPos.z += playerSpeed;
-        if (IsKeyDown(KEY_UP)) playerPos.z -= playerSpeed;
-        if (IsKeyDown(KEY_COMMA)) playerPos.y += playerSpeed;
-        if (IsKeyDown(KEY_PERIOD)) playerPos.y -= playerSpeed;
-        // else if (IsKeyPressed(KEY_SPACE)) PlaySound(sound);
+        if (IsKeyDown(KEY_RIGHT)) playerUnit1.position.x += playerUnit1.speed;
+        if (IsKeyDown(KEY_LEFT)) playerUnit1.position.x -= playerUnit1.speed;
+        if (IsKeyDown(KEY_DOWN)) playerUnit1.position.z += playerUnit1.speed;
+        if (IsKeyDown(KEY_UP)) playerUnit1.position.z -= playerUnit1.speed;
 
         collision = false;
   
         // Check collisions player vs enemy-box
         if (CheckCollisionBoxes(
-            (BoundingBox){(Vector3){ playerPos.x - playerSize.x/2,
-                                     playerPos.y - playerSize.y/2,
-                                     playerPos.z - playerSize.z/2 },
-                          (Vector3){ playerPos.x + playerSize.x/2,
-                                     playerPos.y + playerSize.y/2,
-                                     playerPos.z + playerSize.z/2 }},
-            (BoundingBox){(Vector3){ enemyBoxPos.x - enemyBoxSize.x/2,
-                                     enemyBoxPos.y - enemyBoxSize.y/2,
-                                     enemyBoxPos.z - enemyBoxSize.z/2 },
-                          (Vector3){ enemyBoxPos.x + enemyBoxSize.x/2,
-                                     enemyBoxPos.y + enemyBoxSize.y/2,
-                                     enemyBoxPos.z + enemyBoxSize.z/2 }})) collision = true;
+            (BoundingBox){(Vector3){ playerUnit1.position.x - playerUnit1.size.x/2,
+                                     playerUnit1.position.y - playerUnit1.size.y/2,
+                                     playerUnit1.position.z - playerUnit1.size.z/2 },
+                          (Vector3){ playerUnit1.position.x + playerUnit1.size.x/2,
+                                     playerUnit1.position.y + playerUnit1.size.y/2,
+                                     playerUnit1.position.z + playerUnit1.size.z/2 }},
+            (BoundingBox){(Vector3){ wall.position.x - 2.0f/2,
+                                     wall.position.y - 2.0f/2,
+                                     wall.position.z - 2.0f/2,},
+                          (Vector3){ wall.position.x + 2.0f/2,
+                                     wall.position.y + 2.0f/2,
+                                     wall.position.z + 2.0f/2,}})) collision = true;
 
         if (collision) 
         {
-            playerColor = RED;
-            playerPos = oldPlayerPos;
-            // PlaySound(sound);
-            if (IsKeyDown(KEY_P)) // HOLD P TO PUSH OBJECT
-            {
-                if (IsKeyDown(KEY_RIGHT)) playerPos.x += playerSpeed/2, enemyBoxPos.x += playerSpeed/2;
-                else if (IsKeyDown(KEY_LEFT)) playerPos.x -= playerSpeed/2, enemyBoxPos.x -= playerSpeed/2;
-                else if (IsKeyDown(KEY_DOWN)) playerPos.z += playerSpeed/2, enemyBoxPos.z += playerSpeed/2;
-                else if (IsKeyDown(KEY_UP)) playerPos.z -= playerSpeed/2, enemyBoxPos.z -= playerSpeed/2;
-                else if (IsKeyDown(KEY_COMMA)) playerPos.y += playerSpeed/2, enemyBoxPos.y += playerSpeed/2;
-                else if (IsKeyDown(KEY_PERIOD)) playerPos.y -= playerSpeed/2, enemyBoxPos.y -= playerSpeed/2;
-            }
+            playerUnit1.colour = RED;
+            resolveCollision(&playerUnit1, wall.position);
         }
-        else playerColor = GREEN;
+        else playerUnit1.colour = GREEN;
 
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
@@ -87,27 +88,26 @@ int main(void)
             BeginMode3D(camera);
 
                 // Draw enemy-box
-                DrawCube(enemyBoxPos, enemyBoxSize.x, enemyBoxSize.y, enemyBoxSize.z, GRAY);
-                DrawCubeWires(enemyBoxPos, enemyBoxSize.x, enemyBoxSize.y, enemyBoxSize.z, DARKGRAY);
+                DrawCube(wall.position, 2.0f, 2.0f, 2.0f, GRAY);
+                DrawCubeWires(wall.position, 2.0f/2, 2.0f/2, 2.0f/2, DARKGRAY);
 
                 // Draw player
-                DrawCubeV(playerPos, playerSize, playerColor);
+                DrawCubeV(playerUnit1.position, playerUnit1.size, playerUnit1.colour);
 
-                DrawGrid(10, 1.0f);        // Draw a grid
+                DrawGrid(10, TILE_SIZE);  // i think... 
 
             EndMode3D();
 
-            DrawText("Hold P + directional key to push", 220, 40, 20, GRAY);
-
             DrawFPS(10, 10);
+            DrawText(TextFormat("player pos X = %f  \n\nY= %f  \n\nZ = %f", playerUnit1.position.x, playerUnit1.position.y, playerUnit1.position.z), 30, 30, 30, DARKGRAY);
+            DrawText(TextFormat("wall coordinates X = %f  \n\nY= %f  \n\nZ = %f", wall.position.x, wall.position.x, wall.position.x), 30, 300, 30, DARKGRAY);
+
+
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
     CloseWindow();        // Close window and OpenGL context
-    // CloseAudioDevice();
 
     return 0;
 }
