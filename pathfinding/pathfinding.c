7f14a5d928bfce55995437f4038282a3dd0de3ae
Aaron <117947303+aaronbehan@users.noreply.github.com>
2025-01-20 13:15:36 +0000

    Update pathfinding.c
    Heavily annotated GPT script so that I can better understand. I adapted it to Raylib and made each cycle of the algorithm dependent upon spacebar input and coloured the nodes. This helped me to understand how the algorithm can be structured. 
    
    Next steps will be further modification and marriage with already developed mechanics.

--- pathfinding/pathfinding.c
+++ pathfinding/pathfinding.c
@@ -5,227 +5,214 @@
 #include "structs.h"
 #include "functions.h"
 
+#define GRID_WIDTH 10
+#define GRID_HEIGHT 10
 #define TILE_SIZE 1.0f
-#define ORTHOGONAL_COST 1.0f
-#define DIAGONAL_COST 1.4f
 
-// Function to calculate Diagonal Distance heuristic (best for my program as it uses graphs)
-int calculateHCost(PlaneVector start, PlaneVector goal) 
-{
-    int dx = abs(goal.x - start.x);  // returns the difference and stores it in dx
-    int dy = abs(goal.z - start.z);
-    float HCostFloat = (ORTHOGONAL_COST * (dx > dy ? dx : dy)) + (DIAGONAL_COST - ORTHOGONAL_COST) * (dx < dy ? dx : dy);
-    return (int){HCostFloat * 10};  // making the variable simpler to work with
+// Function to calculate the heuristic (Euclidean distance)
+float calculateHeuristic(Node* a, Node* b) {
+    return sqrtf((a->x - b->x) * (a->x - b->x) + (a->y - b->y) * (a->y - b->y));
 }
 
-// could not think of a better way to do this
-void initPFNodeNeighbours(PlaneVector parentPosition, PlaneVector targetPosition, int parentGCost, PFNodeList open) 
+// Function to find the node with the lowest fCost in the open list
+Node* findLowestFCostNode(Node** openList, int openListSize) // whenever an array is passed as an argument, the array decays to a pointer to its first element. hence the pointer to a pointer Node**
 {
-    PathFindingNode north;
-    north.position.x = parentPosition.x;
-    north.position.z = parentPosition.z - TILE_SIZE;
-
-    // check open, closed and walls
-
-    north.parentPosition = parentPosition;
-    north.GCost = parentGCost + 10;
-    north.HCost = calculateHCost((PlaneVector){north.position.x, north.position.z}, targetPosition);
-    north.FCost = north.GCost + north.HCost;
-    open.pointer[0] = north;
-
-    PathFindingNode northEast;
-    northEast.position.x = parentPosition.x + TILE_SIZE;
-    northEast.position.z = parentPosition.z - TILE_SIZE;
-    northEast.parentPosition = parentPosition;
-    northEast.GCost = parentGCost + 14;
-    northEast.HCost = calculateHCost((PlaneVector){northEast.position.x, northEast.position.z}, targetPosition);
-    northEast.FCost = northEast.GCost + northEast.HCost;
-    open.pointer[1] = northEast;
-
-    PathFindingNode east;
-    east.position.x = parentPosition.x + TILE_SIZE;
-    east.position.z = parentPosition.z;
-    east.parentPosition = parentPosition;
-    east.GCost = parentGCost + 10;
-    east.HCost = calculateHCost((PlaneVector){east.position.x, east.position.z}, targetPosition);
-    east.FCost = east.GCost + east.HCost;
-    open.pointer[2] = east;
-
-    PathFindingNode southEast;
-    southEast.position.x = parentPosition.x + TILE_SIZE;
-    southEast.position.z = parentPosition.z + TILE_SIZE;
-    southEast.parentPosition = parentPosition;
-    southEast.GCost = parentGCost + 14;
-    southEast.HCost = calculateHCost((PlaneVector){southEast.position.x, southEast.position.z}, targetPosition);
-    southEast.FCost = southEast.GCost + southEast.HCost;
-    open.pointer[3] = southEast;
-
-    PathFindingNode south;
-    south.position.x = parentPosition.x;
-    south.position.z = parentPosition.z + TILE_SIZE;
-    south.parentPosition = parentPosition;
-    south.GCost = parentGCost + 10;
-    south.HCost = calculateHCost((PlaneVector){south.position.x, south.position.z}, targetPosition);
-    south.FCost = south.GCost + south.HCost;
-    open.pointer[4] = south;
-
-    PathFindingNode southWest;
-    southWest.position.x = parentPosition.x - TILE_SIZE;
-    southWest.position.z = parentPosition.z + TILE_SIZE;
-    southWest.parentPosition = parentPosition;
-    southWest.GCost = parentGCost + 14;
-    southWest.HCost = calculateHCost((PlaneVector){southWest.position.x, southWest.position.z}, targetPosition);
-    southWest.FCost = southWest.GCost + southWest.HCost;
-    open.pointer[5] = southWest;
-
-    PathFindingNode west;
-    west.position.x = parentPosition.x - TILE_SIZE;
-    west.position.z = parentPosition.z;
-    west.parentPosition = parentPosition;
-    west.GCost = parentGCost + 10;
-    west.HCost = calculateHCost((PlaneVector){west.position.x, west.position.z}, targetPosition);
-    west.FCost = west.GCost + west.HCost;
-    open.pointer[6] = west;
-
-    PathFindingNode northWest;
-    northWest.position.x = parentPosition.x - TILE_SIZE;
-    northWest.position.z = parentPosition.z - TILE_SIZE;
-    northWest.parentPosition = parentPosition;
-    northWest.GCost = parentGCost + 14;
-    northWest.HCost = calculateHCost((PlaneVector){northWest.position.x, northWest.position.z}, targetPosition);
-    northWest.FCost = northWest.GCost + northWest.HCost;
-    open.pointer[7] = northWest;
-
-    //check the intended position against closed and walls (you must indeed check open because you must overwrite if it yields a lower F cost) 
-
-    // for i < 8:
-        // iterate through the positions of open, checking for duplicates.
-        // if duplicate found: check its F cost and replace it with the new one if it is lower
-        // check the closed array for duplicates
-
-    // realloc with ~8 extra spaces for open?
-
-    // put current node into closed
+    Node* lowest = openList[0];  // asserts that the lowest F cost is openList[0]
+    for (int i = 1; i < openListSize; i++) // then checks that fact against all other F costs in openList[]
+    {
+        if (openList[i]->fCost < lowest->fCost || 
+           (openList[i]->fCost == lowest->fCost && openList[i]->hCost < lowest->hCost)) {  // lower H cost is the deciding factor when considering F costs of equivalent values
+            lowest = openList[i];
+        }
+    }
+    return lowest;
 }
 
-// Pseudo code
-// OPEN // array of nodes to be evaluated
-// CLOSED // array of nodes already evaluated
-// add the start node to OPEN
- 
-// loop
-// current = node in OPEN with the lowest f_cost
-// remove current from OPEN
-// add current to CLOSED
-
-// if current is the target node //path has been found
-//     return
-
-// for each neighbour of the current node
-//     if neighbour is not traversable or neighbour is in CLOSED
-//         skip to the next neighbour
-
-//     if new path to neighbour is shorter OR neighbour is not in OPEN
-//         set f_cost of neighbour
-//         set parent of neighbour to current
-//         if neighbour is not in OPEN
-//             add neighbour to OPEN
-
-PlaneVector* FindPath(PlaneVector currentPosition, PlaneVector targetPosition) // must be a pointer because we don't know how many parent nodes it will have taken in order to relay the path
+// Checks if the node is present in the list
+int isNodeInList(Node** list, int listSize, Node* node) 
 {
-    PlaneVector* path = { 0 };
-
-    return path;
+    for (int i = 0; i < listSize; i++) 
+    {
+        if (list[i]->x == node->x && list[i]->y == node->y) {  // this can be simplified through use of vector struct
+            return 1;
+        }
+    }
+    return 0;
 }
 
-int main(void) 
+// Get neighbors of a node                              
+int getNeighbors(Node grid[GRID_WIDTH][GRID_HEIGHT], Node* currentNode, Node** neighbors) {
+    int count = 0;
+    for (int dx = -1; dx <= 1; dx++) {  // starts at the west
+        for (int dy = -1; dy <= 1; dy++) // south corner (southwest)
+        {  
+            if (dx == 0 && dy == 0) continue; // Skip the current node
+
+            int nx = currentNode->x + dx;  // taking currentNode.x, adding dx (which could be a negative number) to it  and then storing it in nx
+            int ny = currentNode->y + dy;  // taking currentNode.y, adding dy to it and then storing it in ny
+
+            //    <<<<<ensure it is within the bounds of the map>>>>>>>         ensure not a wall
+            if (nx >= 0 && nx < GRID_WIDTH && ny >= 0 && ny < GRID_HEIGHT && grid[nx][ny].walkable) {
+                neighbors[count++] = &grid[nx][ny];  // add the node at grid[nx][ny] to neighbors and increment count by 1
+            }
+        }
+    }
+    return count;
+}
+
+// Reconstruct the path from the end node to the start node
+void reconstructPath(Node* endNode) 
+{
+    int numberOfNodes = 0;  // incrememt this with each loop iteration to inform malloc
+    Node* current = endNode;
+    while (current != NULL) // turn into a for loop which populates a pointer of nodes
+    {
+        printf("(%d, %d) ", current->x, current->y);
+        current = current->parent; // the parent of the very first node would be null
+    }
+    printf("\n");  // then return the pointer
+}
+
+// A* algorithm  THE ENTIRE PROGRAM IS BASICALLY JUST THIS FUNCTION======================================
+void aStar(Node grid[GRID_WIDTH][GRID_HEIGHT], Node* startNode, Node* endNode) 
+{
+
+    printf("No path found!\n");  // return null is better
+}
+
+int main(void)
 {
     const int screenWidth = 1400;
     const int screenHeight = 850;
-    InitWindow(screenWidth, screenHeight, "raylib path finding");
-    Camera camera = { { 0.0f, 10.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 };
+    InitWindow(screenWidth, screenHeight, "raylib pf");
+    Camera camera = { { 5.0f, 10.0f, 10.0f }, { 5.0f, 0.0f, 5.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 };
 
-    // Start and end nodes
-    PathFindingNode startNode = {{ -2.5, -2.5 }, { 0 }, 0, 0, 0 }; 
-    PathFindingNode endNode = {{ 1.5, 0.5 }, { 0 }, 0, 0, 0 };
+    Node grid[GRID_WIDTH][GRID_HEIGHT];
 
-    PFNodeList openNodes;
-    openNodes.counter = 8;
-    openNodes.pointer = (PathFindingNode*)malloc(openNodes.counter * sizeof(PathFindingNode));
+    // Initialize the grid. Goes through every node in the grid and assigns it:
+    for (int x = 0; x < GRID_WIDTH; x++) {
+        for (int y = 0; y < GRID_HEIGHT; y++) {
+            grid[x][y].x = x;  // coordinates
+            grid[x][y].y = y;
+            grid[x][y].gCost = grid[x][y].hCost = grid[x][y].fCost = 0;  // costs
+            grid[x][y].parent = NULL;
+            grid[x][y].walkable = 1; // All nodes are walkable by default
+        }
+    }
 
-    initPFNodeNeighbours(startNode.position, endNode.position, startNode.GCost, openNodes);
+    // Reassigning some tiles to walls
+    grid[4][4].walkable = 0;
+    grid[4][5].walkable = 0;
+    grid[4][6].walkable = 0;
 
-    // PFNodeList closedNodes = {NULL, 0};
+    //Node* debuggingNode;  // debuggingNode = currentNode. in order to easily print things like h cost, f cost, g cost. drawtext(debuggingnode.hcost etc)
 
-    SetTargetFPS(60);  // Set our game to run at 60 frames-per-second
+    Node* startNode = &grid[0][0];
+    Node* endNode = &grid[9][9];
 
-    int debugging_int = 0;
+    // --------------------------------------------------------------------------------------------
+    //aStar(grid, startNode, endNode);
+    // --------------------------------------------------------------------------------------------
+
+    Node* openList[GRID_WIDTH * GRID_HEIGHT];  // CREATING AN ARRAY WITH THE TOTAL AMOUNT OF TILES AS ITS SIZE
+    int openListSize = 0;
+
+    Node* closedList[GRID_WIDTH * GRID_HEIGHT];  // CREATING AN ARRAY WITH THE TOTAL AMOUNT OF TILES AS ITS SIZE
+    int closedListSize = 0;
+
+    openList[openListSize++] = startNode;  // putting the first node into the start list and then incrementing by 1?
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Calculate heuristics
-        int hCost = calculateHCost(startNode.position, endNode.position);
+        if (IsKeyDown(KEY_W)) camera.position.z -= 0.1, camera.target.z -= 0.1;
+        if (IsKeyDown(KEY_A)) camera.position.x -= 0.1, camera.target.x -= 0.1;
+        if (IsKeyDown(KEY_S)) camera.position.z += 0.1, camera.target.z += 0.1;
+        if (IsKeyDown(KEY_D)) camera.position.x += 0.1, camera.target.x += 0.1;
 
-        //start node controls
-        if (IsKeyPressed(KEY_RIGHT)) startNode.position.x += TILE_SIZE;
-        if (IsKeyPressed(KEY_LEFT)) startNode.position.x -= TILE_SIZE;
-        if (IsKeyPressed(KEY_DOWN)) startNode.position.z += TILE_SIZE;
-        if (IsKeyPressed(KEY_UP)) startNode.position.z -= TILE_SIZE;        
-        //end node controls
-        if (IsKeyPressed(KEY_D)) endNode.position.x += TILE_SIZE;
-        if (IsKeyPressed(KEY_A)) endNode.position.x -= TILE_SIZE;
-        if (IsKeyPressed(KEY_S)) endNode.position.z += TILE_SIZE;
-        if (IsKeyPressed(KEY_W)) endNode.position.z -= TILE_SIZE;
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            Node* currentNode = findLowestFCostNode(openList, openListSize);  // assigns current node to node with lowest F cost
 
-        if (IsKeyPressed(KEY_Q)) debugging_int -= 1;
-        if (IsKeyPressed(KEY_E)) debugging_int += 1;
+            // Remove currentNode from openList
+            for (int i = 0; i < openListSize; i++) // iterates through openList
+            {
+                if (openList[i] == currentNode) // it finds the currentNode inside openList
+                {
+                    openList[i] = openList[--openListSize];  // it takes the very last element in openList and puts it at the address currentNode was found at. Then decreases size of openList by 1. Not so convinced by this
+                    break;
+                }
+            }
 
-        // Draw
+            closedList[closedListSize++] = currentNode;  // adds currentNode to closedList and increases closedListSize by 1
+
+            if (currentNode == endNode) {  // exit function if currentNode is endNode
+                reconstructPath(endNode);
+                return;
+            }
+
+            Node* neighbors[8];  // else: create a pointer to an array of 8 neighbours
+            int neighborCount = getNeighbors(grid, currentNode, neighbors);  // stores 8 nodes (that neighbour currentNode) in neighbours array and returns number of neighbours created. only neighbours that are out of bounds or inside a wall do not get created
+
+            for (int i = 0; i < neighborCount; i++) // iterates through the newly made neighbours array
+            {
+                Node* neighbor = neighbors[i];  
+
+                if (isNodeInList(closedList, closedListSize, neighbor)) {
+                    continue;  // continuing here means skip everything below it and move onto the next iteration
+                }
+
+                float tentativeGCost = currentNode->gCost + calculateHeuristic(currentNode, neighbor);  // this does work for getting the g cost. you take the g cost from parent node and add 1 or 1.4. i'm guessing these values are returned from the calculateH func
+
+                if (!isNodeInList(openList, openListSize, neighbor)) // if node is not in the openList: add it to the open list. !!!!!!! significant step!
+                {  
+                    openList[openListSize++] = neighbor;
+                } else if (tentativeGCost >= neighbor->gCost) {  // if node is in the list and the newly calculated gcost is greater than its previously calculated gcost, skip to next iteration
+                    continue;
+                }
+
+                // initialise its members 
+                neighbor->gCost = tentativeGCost;
+                neighbor->hCost = calculateHeuristic(neighbor, endNode);
+                neighbor->fCost = neighbor->gCost + neighbor->hCost;
+                neighbor->parent = currentNode;
+            }
+        }
+
+        // float axisZ = ((GRID_WIDTH * -1) / 2) + 0.5f;    // to offset the nodes
+        // float axisX = ((GRID_HEIGHT * -1) / 2) + 0.5f;
+
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
             BeginMode3D(camera);
 
-                DrawCube((Vector3){startNode.position.x, 0, startNode.position.z}, TILE_SIZE, 0.1, TILE_SIZE, BLUE);
-                DrawCube((Vector3){endNode.position.x, 0, endNode.position.z}, TILE_SIZE, 0.1, TILE_SIZE, LIGHTGRAY);
+                // walls
+                DrawCube((Vector3){4, 0.5, 4}, 0.75f, 1.0f, 0.75f, GRAY);
+                DrawCube((Vector3){4, 0.5, 5}, 0.75f, 1.0f, 0.75f, GRAY);
+                DrawCube((Vector3){4, 0.5, 6}, 0.75f, 1.0f, 0.75f, GRAY);
+                // end node
+                DrawCube((Vector3){9, 0.5, 9}, 0.75f, 1.0f, 0.75f, GOLD);
 
-                for (int i = 0; i < 8; i++) // drawing child nodes
-                {
-                    DrawCube((Vector3){openNodes.pointer[i].position.x, 0, openNodes.pointer[i].position.z}, TILE_SIZE, 0.1, TILE_SIZE, LIGHTGRAY);
+                for (int i = 0; i < openListSize; i++) {
+                    DrawCube((Vector3){openList[i]->x, 0.05, openList[i]->y}, 0.75f, 0.1f, 0.75f, GREEN);
+                }                
+                
+                for (int i = 0; i < closedListSize; i++) {
+                    DrawCube((Vector3){closedList[i]->x, 0.05, closedList[i]->y}, 0.75f, 0.1f, 0.75f, RED);
                 }
-                // DrawCube((Vector3){openNodes.pointer[0].position.x, 0, openNodes.pointer[0].position.z}, TILE_SIZE, 0.1, TILE_SIZE, LIGHTGRAY);
-                // DrawCube((Vector3){openNodes.pointer[1].position.x, 0, openNodes.pointer[1].position.z}, TILE_SIZE, 0.1, TILE_SIZE, LIGHTGRAY);
 
-                DrawCubeWires((Vector3){openNodes.pointer[debugging_int].position.x, 0, openNodes.pointer[debugging_int].position.z}, TILE_SIZE + 0.1, 0.15, TILE_SIZE + 0.1, GREEN);
-
-
-                DrawGrid(20, 1.0f);  // Draw a grid
+                DrawGrid(GRID_HEIGHT * 2, 1.0f);        // Draw a grid
 
             EndMode3D();
 
-            // debug
-            DrawText(TextFormat("Start node X = %f, Z = %f", startNode.position.x, startNode.position.z), 20, 40, 30, GRAY);
-            DrawText(TextFormat("End node X = %f, Z = %f", endNode.position.x, endNode.position.z), 20, 75, 30, GRAY);
-            DrawText(TextFormat("dx = %d, dy = %d", (abs(endNode.position.x - startNode.position.x)), abs(endNode.position.z - startNode.position.z)), 20, 110, 30, GRAY);
-
-            DrawText(TextFormat("first half of h cost = %f", (1.0f * ((abs(endNode.position.x - startNode.position.x)) > abs(endNode.position.z - startNode.position.z) ? (abs(endNode.position.x - startNode.position.x)) : abs(endNode.position.z - startNode.position.z)))), 20, 450, 30, GRAY);
-            DrawText(TextFormat("second half of h cost = %f", (0.4f * ((abs(endNode.position.x - startNode.position.x)) < abs(endNode.position.z - startNode.position.z) ? (abs(endNode.position.x - startNode.position.x)) : abs(endNode.position.z - startNode.position.z)))), 20, 485, 30, GRAY);
-            
-            // DrawText(TextFormat("H cost = %d", hCost), 20, 715, 30, GRAY);
-            DrawText(TextFormat("H cost = %d", hCost), 20, 750, 30, GRAY);
-
-
-            DrawText(TextFormat("Selected node's costs:"), 800, 5, 30, GRAY);
-
-            DrawText(TextFormat("G cost = %d", openNodes.pointer[debugging_int].GCost), 800, 35, 30, GRAY);
-            DrawText(TextFormat("H cost = %d", openNodes.pointer[debugging_int].HCost), 800, 70, 30, GRAY);
-            DrawText(TextFormat("F cost = %d", openNodes.pointer[debugging_int].FCost), 800, 115, 30, GRAY);
-            
-            DrawFPS(10, 5);
+            DrawFPS(10, 10);
 
         EndDrawing();
+        //----------------------------------------------------------------------------------
     }
 
     CloseWindow();        // Close window and OpenGL context
